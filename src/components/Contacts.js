cd9737a03ae58d92694fdf38bd7e9262bf7e7941
kevinmisati <kevinmosigisi2001@gmail.com>
2021-07-05 19:36:20 +0300

    changes

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,8 +1,51 @@
-import React from 'react'
+import React,{useState} from 'react'
 import classes from "./Contact.module.css"
 const Contacts = () => {
+  const [emailIsValid, setEmailIsValid] = useState(true)
+  const [email, setEmail] = useState("");
+
+  const [name, setName] = useState("")
+  const [nameIsValid,setNameIsValid] = useState(true)
+  
+  const [message, setMessage] = useState("")
+  const [messageIsValid,setMessageIsValid] = useState(true)
+
+  const handleMessageChange = (e) => {
+    setMessage(e.target.value)
+  }
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value)
+   
+  }
+  const handleNameChange = (e) => {
+    setName(e.target.value)
+  }
   const handleSubmit = (e) => {
-     e.preventDefault()
+    e.preventDefault()
+    if (email.trim().length > 0 && email.includes("@")) {
+      setEmailIsValid(true)
+    }
+    else {
+      setEmailIsValid(false)
+      return;
+    }
+
+    if (name.trim().length > 0 ) {
+      setNameIsValid(true)
+    }
+    else {
+      setNameIsValid(false)
+      return;
+    }
+
+    if (message.trim().length > 0) {
+      setMessageIsValid(true)
+    }
+    else {
+      setMessageIsValid(false)
+      return;
+    }
    }
     return (
       <section id="contact" className={classes["contacts-container"]}>
@@ -15,12 +58,25 @@ const Contacts = () => {
           <form onSubmit={handleSubmit}>
             <div className={classes["container-flex"]}>
               <div className={classes["first-nav-contacts"]}>
-                <input placeholder="Name" name="fname" type="text" />
-                <input placeholder="Email" name="email" type="email" />
+                <input
+                  onChange={handleNameChange}
+                  value={name}
+                  placeholder="Name"
+                  name="fname"
+                  type="text"
+                  className={nameIsValid ? "" :classes.invalid}
+                />
+                <input  className= {emailIsValid ? "": classes.invalid } value={email} onChange = {handleEmailChange} placeholder="Email" name="email" type="email" />
                
               </div>
               <div className={classes["second-nav-contacts"]}>
-                <textarea cols="10" ros="60"></textarea>
+                <textarea
+                  cols="10"
+                  ros="60"
+                  value={message}
+                  onChange= {handleMessageChange}
+                  className={messageIsValid ? "" : classes.invalid}
+                ></textarea>
               </div>
             </div>
             <div className={classes['btn-container']}>
